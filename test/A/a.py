2d5fd183e0fe72d91b5d58234b93435ebc9f904c
chonghyo <86150567+chonghyo@users.noreply.github.com>
2022-07-23 00:40:34 +0900

    Add files via upload

---
+++ test/A/a.py
@@ -0,0 +1,38 @@
+"""
+====================================
+ :mod:`a` module a
+====================================
+.. moduleauthor:: hyo156 <hyo156@kitech.re.kr>
+.. note:: PSID
+
+explanation
+=====
+
+module exam
+
+Revision History
+-------------------
+
+ * [2022/07/22] - module cccc
+"""
+
+class A:
+    """testest
+    """
+    def __init__(self, a: int):
+        """ class gener
+
+        :param a: gener param
+
+        >>> a = A(10)
+        """
+        self.a = a
+
+    def print_value(self):
+        """ a print method
+
+        >>> A(10).print_value()
+        10
+        """
+        print(self.a)
+
